c0ed5a2458c5edc53abe0df9cbcf84f8cd9d7a19
Armond Upshaw <upshaw620@gmail.com>
2024-10-08 22:29:59 -0400

    Add files via upload

---
+++ scripts 1.js
@@ -0,0 +1,12 @@
+
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    if (name == "" || email == "" || message == "") {
+        alert("All fields must be filled out");
+        return false;
+    }
+    return true;
+}
